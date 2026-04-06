2b52b9be6170e2c2491a6bf6e4e6acc13a3bdaed
7x <info@se7enx.com>
2026-04-06 07:35:01 -0700

    fix: legacy_site design.ini - correct CSS/JS filenames (main not main-un)

--- se7enxweb/exponential-platform-dxp/4.6.x-LB-dev/src/ezpublish_legacy/app/settings/siteaccess/legacy_site/design.ini.append.php
+++ se7enxweb/exponential-platform-dxp/4.6.x-LB-dev/src/ezpublish_legacy/app/settings/siteaccess/legacy_site/design.ini.append.php
@@ -5,14 +5,14 @@ SiteCSS=var/storage/packages/eZ-systems/ezwebin_design_blue/files/default/file/s
 ClassesCSS=var/storage/packages/eZ-systems/ezwebin_design_blue/files/default/file/classes-colors.css
 #CSSFileList[]=yui/build/calendar/assets/calendar.css
 #CSSFileList[]=site-colors.css
-CSSFileList[]=main-un.css
-CSSFileList[]=main-un.res.css
+CSSFileList[]=main.css
+CSSFileList[]=main.res.css
 
 [JavaScriptSettings]
 #JavaScriptList[]=insertmedia.js
 #JavaScriptList[]=yui/build/yahoo-dom-event/yahoo-dom-event.js
 #JavaScriptList[]=yui/build/calendar/calendar.js
 #JavaScriptList[]=ezdatepicker.js
-JavaScriptList[]=main-un.js
+JavaScriptList[]=main.js
 
 */ ?>
